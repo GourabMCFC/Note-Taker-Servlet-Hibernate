ed07f2cdbf0a9c8f6c89a641204803be89a5ab6d
GourabMCFC <gaurabmukherjee2001@gmail.com>
2024-09-11 16:56:08 +0530

    Create Delete Servlet

--- src/main/java/servlets/DeletePost.java
+++ src/main/java/servlets/DeletePost.java
@@ -1,20 +1,51 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.google.gson.Gson;
+
+import entities.Note;
+import utils.HibernateUtil;
+
 @MultipartConfig
 public class DeletePost extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Gson gson;
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println(request.getParameter("id"));
+		int id = Integer.parseInt(request.getParameter("id"));
+		Map<String, String> responseMap = new HashMap<String, String>();
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			Note note = session.get(Note.class, id);
+			session.beginTransaction();
+			session.delete(note);
+			session.getTransaction().commit();
+			responseMap.put("success", "deleted");
+		} catch (HibernateException e) {
+			responseMap.put("error", e.getMessage());
+			e.printStackTrace();
+		} finally {
+			response.setContentType("text/JSON");
+			response.getWriter().print(gson.toJson(responseMap));
+		}
+	}
+
+	@Override
+	public void init() throws ServletException {
+		gson = new Gson();
 	}
 
 }
